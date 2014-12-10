871c1e4381243be69bccfaf4ebcdfc5d07e16d00
Jacob S <jacob.s.sachs@gmail.com>
2014-12-09 23:25:30 -0600

    initial buff ideas for status.js

--- app/models/status.js
+++ app/models/status.js
@@ -1,24 +1,25 @@
 var mongoose = require('mongoose');
 
 var statusSchema = mongoose.Schema({
-    region: {type: String, required: true, unique: true},
-    gameStatus: {type: String, required: true}
+  region: {type: String, required: true, unique: true},
+  gameStatus: {type: String, required: true}
 });
 
-
+// TODO have this better designed alongside new documentation
 // executes the callback if game server status has changed for a region
 statusSchema.statics.statusChange = function(reg, newStatus, fn) {
   var Status = this || mongoose.model('Status');
   Status.findOne({region: reg}, function(err, status) {
     if (err) {
-      return fn(new Error(err));
+      console.log(err);
+      return err;
     }
     if (status) {
       if (status.gameStatus == newStatus) return;
       status.update({gameStatus: newStatus}).exec();
       return fn(null, reg, newStatus);
     }
-    // if there is no current status, create the status and return false
+    // if there is no current status, create the status
     Status.create({
       region : reg,
       gameStatus : newStatus,
