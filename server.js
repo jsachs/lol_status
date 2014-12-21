e437069635c8373513dd3dd7ea4d22814fdba8c3
Jacob S <jacob.s.sachs@gmail.com>
2014-12-21 17:44:22 -0600

    framework for a better README

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var methodOverride = require('method-override');      // simulate DELETE and PUT
 var database = require('./config');
 mongoose.connect(database.db.url);
 
-app.use(express.static(__dirname + '/public')); 				        // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); 				        // set the static files location
 app.use(morgan('dev')); 										                    // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); 			      // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); 									                  // parse application/json
